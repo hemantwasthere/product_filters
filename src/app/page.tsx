7e0938809ff643dc4555b6783dbb3ac4a527458c
Hemant <hrsmart2150@gmail.com>
2024-04-14 00:23:32 +0530

    removed hardcoded url for api route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -125,7 +125,7 @@ const Home: NextPage = () => {
     queryKey: ["products"],
     queryFn: async () => {
       const { data } = await axios.post<QueryResult<TProduct>[]>(
-        "http://localhost:3000/api/products",
+        "/api/products",
         {
           filter: {
             sort: filter.sort,
